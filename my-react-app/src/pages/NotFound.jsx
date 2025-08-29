690fa096e7b6582693a25fc0e9500d8396f386cb
lasquale619 <med.bouguellid@hotmail.com>
2025-08-29 20:21:43 +0200

    Mise en place de la page 404

--- my-react-app/src/pages/NotFound.jsx
+++ my-react-app/src/pages/NotFound.jsx
@@ -1,9 +1,12 @@
-const NotFound = () => {
-    return (
-        <div>
-            Erreur 404
-        </div>
-    );
-};
+import { Link } from "react-router-dom";
+import "@/Components/styles/notfound.css";
 
-export default NotFound;
+export default function NotFound() {
+    return(
+        <section className="not-found">
+            <h1>404</h1>
+            <p>Oups! La page que vous demandez n'existe pas.</p>
+            <Link to="/" className="nf-link">Retourner sur la page d’accueil</Link>
+        </section>
+    )
+}
